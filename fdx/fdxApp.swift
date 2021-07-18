d99fbecc7507f09c8aec6e8b8f7d94dc96f7ad5e
shimizu.kenji <shimizu.kenji@noncore.net>
2021-07-18 19:47:59 +0900

    create project

---
+++ fdx/fdxApp.swift
@@ -0,0 +1,17 @@
+//
+//  fdxApp.swift
+//  fdx
+//
+//  Created by shimizu kenji on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct fdxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
